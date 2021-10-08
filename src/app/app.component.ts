8c20891e05daa08591c2d4f87d7d2b0d851d466d
Javier Donadio <javier.prog@gmail.com>
2021-10-08 11:26:20 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pretty-angular';
+  title = "'pretty-angular' pretty 'angular'";
+
+  customFuction() {
+    console.log('customFuction');
+    const a = {
+      1: 1,
+      2: 1,
+    };
+  }
 }
